9827dd9e1e35bd7ca0e4506d0dc9058fdac0424a
dreadedapparatus <richardastarr@gmail.com>
2025-10-06 22:34:33 -0400

    final fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // The name of your repository.
 // This is used to set the base path for the deployed application.
-const GITHUB_PAGES_BASE_PATH = '/PromoWidget2/';
+const GITHUB_PAGES_BASE_PATH = '/promo-widget-gen/';
 
 // https://vitejs.dev/config/
 export default defineConfig({
